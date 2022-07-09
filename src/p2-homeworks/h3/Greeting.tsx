f74d638f1fa6f7f467f2f6f0c6d17683b0275d38
MonroeLionel <MonroeLionel@gmail.com>
2022-07-09 14:00:07 +0300

    Задача от Игната омер 3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import s from './Greeting.module.css'
+import * as events from "events";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+   name: string
+   setNameCallback: (e: React.ChangeEvent<HTMLInputElement>) => void
+   addUser: () => void
+   error: string
+   totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+  {name, setNameCallback, addUser, error, totalUsers}
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+   const inputClass = error ? s.error : s.someClass
+   const disabledHandler = error ? true : false
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
+   return (
+     <div>
+        <input
+          value={name}
+          onChange={setNameCallback}
+          className={inputClass}/>
+        <span>{error}</span>
+        <button disabled={disabledHandler} onClick={addUser}>add</button>
+        <span>{totalUsers}</span>
+     </div>
+   )
 }
 
 export default Greeting
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,34 +1,36 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from "uuid";
 
-// types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+   _id: string
+   name: string
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+   const [users, setUsers] = useState<Array<UserType>>([])
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
-    }
+   const addUserCallback = (name: string) => {
+      let a = {_id: v1(), name: name}
 
-    return (
-        <div>
-            <hr/>
-            homeworks 3
+      setUsers([...users, a])
+   }
 
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+   return (
+     <div>
+        <hr/>
+        homeworks 3
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeGreeting/>*/}
-            <hr/>
-        </div>
-    )
+        {/*should work (должно работать)*/}
+        <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+
+        <hr/>
+        {/*для личного творчества, могу проверить*/}
+        {/*<AlternativeGreeting/>*/}
+        <hr/>
+     </div>
+   )
 }
 
 export default HW3
